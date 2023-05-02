3ce652a317ca5e774ef28c82d248ffbc6379228c
JagMan177 <jagjitmangar177@gmail.com>
2023-05-02 16:13:22 -0400

    commit pt 2.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material';
+//import {Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      App
+      <CssBaseline />
+      <h1>Hello World! - Filmpire!</h1>
     </div>
   )
 }
