2dc3a00ba8bd08d23e19cb8b417a068f9480bb51
JagMan177 <jagjitmangar177@gmail.com>
2023-05-03 12:15:57 -0400

    folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import { CssBaseline } from '@mui/material';
-//import {Route, Switch } from 'react-router-dom';
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+import {Actors, MovieInformation, Movies, NavBar, Profile} from './'
 
 const App = () => {
   return (
     <div>
       <CssBaseline />
-      <h1>Hello World! - Filmpire!</h1>
+      <NavBar/>
+      <main>
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation></MovieInformation>
+          </Route>
+
+          <Route exact path="/actors/:id">
+            <Actors/>
+          </Route>
+
+          <Route exact path="/">
+           <Movies/>
+          </Route>
+
+          <Route exact path="/profile/:id">
+            <Profile/>
+          </Route>
+        </Switch>
+      </main>
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
-
